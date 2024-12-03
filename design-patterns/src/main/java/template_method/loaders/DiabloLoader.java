b623a1fbee119ed900ee2d8b32887a3a1f9cc2f8
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2024-12-03 16:11:52 +0330

    Template Method Design Pattern

---
+++ design-patterns/src/main/java/template_method/loaders/DiabloLoader.java
@@ -0,0 +1,26 @@
+package template_method.loaders;
+
+import template_method.BaseGameLoader;
+
+public class DiabloLoader extends BaseGameLoader {
+    @Override
+    protected byte[] loadLocalData() {
+        System.out.println("Diablo: Loading Local Data...");
+        return new byte[0];
+    }
+
+    @Override
+    protected void createObjects(byte[] data) {
+        System.out.println("Diablo: Creating objects from data...");
+    }
+
+    @Override
+    protected void downloadAdditionalFiles() {
+        System.out.println("Diablo: Downloading additional files...");
+    }
+
+    @Override
+    protected void initializeProfiles() {
+        System.out.println("Diablo: Initializing profiles...");
+    }
+}
