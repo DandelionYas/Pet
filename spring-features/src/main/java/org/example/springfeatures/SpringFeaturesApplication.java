94f6d751e3b186c9870ce2f137bdfbf3faa67f6f
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2025-06-01 12:19:35 +0330

    Add new module Spring Features

---
+++ spring-features/src/main/java/org/example/springfeatures/SpringFeaturesApplication.java
@@ -0,0 +1,13 @@
+package org.example.springfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeaturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFeaturesApplication.class, args);
+    }
+
+}
